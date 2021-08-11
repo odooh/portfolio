7da4f1461abf86fbebe1ab3b6eacca1bfdb0d7ee
odooh84 <kyw084@naver.com>
2021-08-11 23:28:46 +0900

    portfolio

--- scripts/menu.js
+++ scripts/menu.js
@@ -12,6 +12,26 @@ function clickMenu() {
         }
         
 
-var menu = document.getElementById('menu');
+function showL1() {
+    let click = document.getElementById("lettering1-content");
+    if(click.style.display === "none"){
+        click.style.display = "block";
+        console.log("wk");
+    }else{
+        click.style.display = "none";
+        console.log("wk");
+    
+    }
 
+}
+function qcontent() {
+    let click = document.getElementById("lettering1-content");
+    click.style.display = "none";
+}
+var menu = document.getElementById('menu');
 menu.addEventListener("click", clickMenu);
+var lettering1 = document.getElementById("lettering1");
+lettering1.addEventListener("click", showL1);
+
+var x_btn = document.getElementById('x_btn');
+x_btn.addEventListener("click", qcontent)
